94cd51d2503ec78c550b6caeea424df3a9382575
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-09-27 09:03:18 -0400

    Initial commit with aboutMe and window prompt features

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import './style.css'
+const numberOfCupsOfCoffee = 2
+
+const aboutMe = {
+  fullName: 'Jonathon Kieran Childs',
+  luckyNumber: 4,
+  favoriteMovies: ['1942', ' Enders Game', ' Jumanji', ' Godzilla'],
+}
+
+console.log(`${aboutMe.favoriteMovies[1]}`)
+
+const userName = window.prompt('What is your name?', 'Bob')
+
+console.log(`Hello, ${userName}`)
