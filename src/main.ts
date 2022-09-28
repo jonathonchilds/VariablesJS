eaf90e42229bcb66f32500d06f145df9c4bed4c0
jonathonchilds <33766594+jonathonchilds@users.noreply.github.com>
2022-09-27 20:40:39 -0400

    Adds math operations

--- src/main.ts
+++ src/main.ts
@@ -12,3 +12,23 @@ console.log(`${aboutMe.favoriteMovies[1]}`)
 const userName = window.prompt('What is your name?', 'Bob')
 
 console.log(`Hello, ${userName}`)
+
+const firstOperand = Number(window.prompt('Please enter a number:'))
+
+const secondOperand = Number(window.prompt('Please enter another number:'))
+
+const sum = firstOperand + secondOperand
+
+const difference = secondOperand - firstOperand
+
+const product = firstOperand * secondOperand
+
+const quotient = firstOperand / secondOperand
+
+const remainder = firstOperand % secondOperand
+
+console.log(`${sum}`)
+console.log(`${difference}`)
+console.log(`${product}`)
+console.log(`${quotient}`)
+console.log(`${remainder}`)
